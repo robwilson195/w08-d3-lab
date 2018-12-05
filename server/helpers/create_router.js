c5734f2aed535ae1d7a388608f737e252aca1ca7
Alex Clements <alexfjclements@gmail.com>
2018-12-05 11:03:39 +0000

    Index created in create_router

---
+++ server/helpers/create_router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+
+  const router = epxress.Router();
+
+  // Index
+  router.get('/', (req, res) => {
+    collection
+    .find()
+    .toArray()
+    .then((docs) => res.json(docs));
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
+  });
+
+
+
+  return router;
+
+};
+
+module.exports = createRouter;
