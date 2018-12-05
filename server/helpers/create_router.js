10017eed150749b9576167e5ee96b05797ee2815
Alex Clements <alexfjclements@gmail.com>
2018-12-05 11:33:55 +0000

    update route created and tested successful

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -46,7 +46,20 @@ const createRouter = function (collection) {
     });
   });
 
-
+// Update
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+  collection
+  .updateOne({_id: ObjectID(id)}, {$set: updatedData})
+  .then(() => collection.find().toArray())
+  .then((docs) => res.json(docs))
+  .catch((err) => {
+    console.error(err);
+    res.status(500);
+    res.json({status: 500, error: err});
+  });
+});
 
   return router;
 
